a722c4e21ff4ba203aae566e76feefbb1f2978b0
Agraynel <Yi Chen @ Cornell>
2017-11-22 15:53:08 -0500

    Upload search functions
    Fyi dont change the content in board.php, just change in data.txt

--- board.php
+++ board.php
@@ -6,103 +6,104 @@
    <link rel="stylesheet" href="styles/all.css" type="text/css">
    <link rel="icon" href="images/icon.png">
  </head>
-  <?php include("includes/header.php"); ?>
+<?php
+  include("includes/header.php");
+
+  // implement search functionality
+  include("member.php");
+	//search if submit button is pressed
+	if (isset($_POST['search_submit'])) {
+		$file_pointer = fopen("files/data.txt", "r");
+		$members = array();
+
+		//filter the input to lowercase and prevent html interruption
+		$searchName = isset($_POST['searchName']) ? htmlentities(strtolower($_POST['searchName'])) : false;
+		$searchNetid = isset($_POST['searchNetid']) ? htmlentities(strtolower($_POST['searchNetid'])) : false;
+
+		// find matching CREDITS: http://www.w3school.com.cn/php/func_string_strtolower.asp
+		while (!feof($file_pointer)) {
+			$line = fgets($file_pointer);
+			$line1 = trim(preg_replace('/\s\s+/', ' ', $line));
+
+      //convert to lower case to compare CREDITS: http://stackoverflow.com/questions/16477098/how-can-i-remove-n-in-string-in-php
+			$lowerCaseLine = strtolower($line1);
+			$lowerCaseMember = explode('\t', $lowerCaseLine);
+
+			$nameMatching = (empty($searchName) || preg_match("/$searchName/", $lowerCaseMember[0]));
+			$netidMatching = (empty($searchNetid) || preg_match("/$searchNetid/", $lowerCaseMember[1]));
+			$isMatch = $nameMatching && $netidMatching;
+			if ($isMatch) {
+				$member = explode('\t', $line);
+				$members[] = new Member($member[0], $member[1], $member[2], $member[3], $member[4], $member[5]);
+			}
+		}
+
+	}
+?>
+
   <div class = "container_events_projects">
    <div class = "container-text_board">
-  <div class="title_image">
-    <img class="title" src="images/cornell_students.jpg" alt = "title">
-  <div class = "header">
-    <h1> Current Cornell University  General Members and Board Members of SBA </h1>
+     <div class="title_image">
+       <img class="title" src="images/cornell_students.jpg" alt = "title">
+      <div class = "header">
+        <h1> Current Cornell University  General Members and Board Members of SBA </h1>
+      </div>
+      <p class="primary_statement">
+      <b> <i> BOARD MEMBERS </i> <br/>
+        These are the members of Cornell's SBA Board for the year 2017-2018.
+        These members are hoping to create an enivoronment where students are
+        motivated to aquire leaderhsip qualities that will help them in their
+        future endeavors and will simulatneously learn about the various
+        consultants that they can have and use to be the most successfull
+        in whatthat they want to do. </p>
     </div>
-          <p class="primary_statement">
-          <b> <i> BOARD MEMBERS </i> <br/>
-            These are the members of Cornell's SBA Board for the year 2017-2018.
-            These members are hoping to create an enivoronment where students are
-            motivated to aquire leaderhsip qualities that will help them in their
-            future endeavors and will simulatneously learn about the various
-            consultants that they can have and use to be the most successfull
-            in whatthat they want to do. </p>
-       </div>
-       
-       <!-- PLEASE Note that the reason many divs are used is for the show and hide effect
-           for the affiliated with CSS that has been done on W3 Schools -->
-         <div class="content-flex_board">
-          <div class = "catalog_main">
-           <div class = "catalog_item">
-                <img class="catalog_image" src="images/member1.jpg" target="_blank"alt ="content-image"> </a>
-              <div class='catalog_description'>
-         			   <div class='show_catalog'>
-         				    <div class='status_catalog'>
-         					    <h3 class='name'>Ogbonnaya Ngwu <br/>
-                        President</h3>
-                    </div>
-                  </div>
-         			<div class='hide_catalog'>
-         				<h4 class='description'>
-                  <p>College of Agriculture and Life Sciences majroing in AEM.
-                  is concentrating in Finance and Resource Economics.He is
-                  committed to educating his peers at Cornell on the power of
-                  corporate sustainability, and hopes to land a career within
-                  ESG.</h4>
-         			</div>
-         		</div>
-          </div>
-          <div class ="catalog_item">
-            <img class="catalog_image" src="images/member2.jpg" target="_blank" alt = "content-image"> </a>
-           <div class='catalog_description'>
-             <div class='show_catalog'>
-               <div class='status_catalog'>
-                 <h3 class='name'>Anthony Ghaly <br/>
-                    Executive Vice President</h3>
-              </div>
-             </div>
-            <div class='hide_catalog'>
-               <h4 class='description'>A sophomore concentrating in Finance
-                and Entrepreneurship, has co-founded a startup called Stilovi
-                centering around the idea of sustainable fashion. </h4>
-             </div>
-           </div>
-         </div>
-         <div class = "catalog_item">
-           <img class="catalog_image" src="images/member3.png" target="_blank" alt = "content-image"> </a>
-           <div class='catalog_description'>
-             <div class='show_catalog'>
-               <div class='status_catalog'>
-                  <h3 class='name'>Eric Jaehyun Kim <br/>
-                   Treasurer </h3>
-              </div>
-            </div>
-            <div class='hide_catalog'>
-               <h4 class='description'>Eric is a junior from Fullerton
-                California looking to creak into an investment and portfolio
-                management role within the finance industry. He hopes to one day
-                invest in the renewable energy sector. Eric is
-                really passionate about making a difference with the club and thus
-                this has gudided his interests in SBA.</h4>
-             </div>
-           </div>
-          </div>
+    <form id="search_form" name="search_form" class="form" method="POST" action="board.php">
+		<!-- Search name and net id fields -->
+  		<div class="first_line">
+  			<input id="searchName" type="text" placeholder="Name" name="searchName" maxlength="25" autofocus pattern="[0-9A-Za-z-_ ]*">
+        <input id="searchNetid" type="text" placeholder="NetID" name="searchNetid" maxlength="10" autofocus pattern="[0-9a-zA-Z]*">
+  		</div>
+  		<div class="basic-profile-form">
+  			<input id="submit" class="button" type="submit" name="search_submit" value="SEARCH">
+  		</div>
+  	</form>
+
+     <!-- PLEASE Note that the reason many divs are used is for the show and hide effect
+         for the affiliated with CSS that has been done on W3 Schools -->
+    <div class="content-flex_board">
+      <div class = "catalog_main">
+      <?php
+	      global $members;
+      // get member information from the class
+    		foreach ($members as $member) {
+    			$name = $member->name;
+    			$netid = $member->netid;
+    			$job = $member->job;
+    			$school = $member->school;
+    			$description = $member->description;
+    			$image = $member->image;
+        ?>
           <div class = "catalog_item">
-            <img class="catalog_image" src="images/member4.jpg" target="_blank" alt = "content-image"> </a>
+            <?php echo '<img class="catalog_image" src="' . $image . '" alt = $name>';?>
             <div class='catalog_description'>
               <div class='show_catalog'>
                 <div class='status_catalog'>
-                  <h3 class='name'>Thiago Bandeira <br/>
-                    VP of Communications
-                  </h3>
+                  <h3 class='name'><?php echo $name ?> (<?php echo $netid ?>)</h3>
+                  <h3 class='name'><?php echo $job ?></h3>
+                  <h3 class='name'><?php echo $school ?></h3>
+              </div>
+              </div>
+         			<div class='hide_catalog'>
+         				<div class='description'>
+                  <h4><?php echo $description ?></h4>
                 </div>
-              </div>
-              <div class='hide_catalog'>
-                <h4 class='description'>Thiago is an enthusiastic mechanical
-                engineer from Seattle, WA looking to take both business and
-                technically sustainable strategies into the aerospace and
-                aeronautical industry.</h4>
-              </div>
-            </div>
-           </div>
+         			</div>
+         		</div>
           </div>
+        <?php } ?>
         </div>
       </div>
     </div>
+  </div>
   <?php include "includes/footer.php";?>
 </html>
