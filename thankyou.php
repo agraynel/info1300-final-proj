527dc39bd70b96135f8e7e24d26051b9ff6812fc
Dongqing Wang <hollywangdq@gmail.com>
2017-11-22 19:32:10 -0500

    Dongqing's changes

---
+++ thankyou.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="UTF-8">
+   <title>Cornell Application</title>
+   <link rel="stylesheet" href="styles/all.css" type="text/css">
+   <link rel="icon" href="images/icon.png">
+ </head>
+  <?php include("includes/header.php"); ?>
+  <p id= "response"> Your response has been recorded. Thank you for your interests in joining us.!<p/>
+  <?php include("includes/footer.php"); ?>
+</html>
