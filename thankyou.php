17d049b468a1d15fb47283f6f8c531e8f2e62423
Agraynel <Yi Chen @ Cornell>
2017-12-03 17:31:31 -0500

    Fix indentations

--- thankyou.php
+++ thankyou.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
+  session_start();
 
-$firstName = $_SESSION['firstname'];
-$lastName = $_SESSION['lastname'];
+  $firstName = $_SESSION['firstname'];
+  $lastName = $_SESSION['lastname'];
 
-$name = $firstName . " " . $lastName;
+  $name = $firstName . " " . $lastName;
 ?>
 
 <!DOCTYPE html>
@@ -19,10 +19,9 @@ $name = $firstName . " " . $lastName;
   <?php include ("includes/header.php"); ?>
   <p id= "response"> To <?php echo($name) ?>: Your response has been recorded. Thank you for your interests in joining us.!<p/>
   <p id="process"> While you await your response form us, here are links you can use to learn more about enivornmental conuslting <br/> <br/>
-  <a href="http://www.sustainabilityconsulting.com/blog/2016/5/18/free-learning-resources-for-aspiring-sustainability-professionals"> Sustainable Consutling 101 </a> <br/> <br/>
-  <a href="http://www.sustainabilityconsulting.com/blog/2016/5/18/free-learning-resources-for-aspiring-sustainability-professionals"> Sustainable Consulting Skills </a>
-</p>
-
+    <a href="http://www.sustainabilityconsulting.com/blog/2016/5/18/free-learning-resources-for-aspiring-sustainability-professionals"> Sustainable Consutling 101 </a> <br/> <br/>
+    <a href="http://www.sustainabilityconsulting.com/blog/2016/5/18/free-learning-resources-for-aspiring-sustainability-professionals"> Sustainable Consulting Skills </a>
+  </p>
   <?php include ("includes/footer.php"); ?>
 </body>
 
