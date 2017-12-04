8f81edc1173fc7a25400d5a0d636e6fbc03b52cd
Agraynel <Yi Chen @ Cornell>
2017-12-03 19:06:46 -0500

    Revert "Merge branch 'yichen-branch'"
    This reverts commit e08d18390140e0e512d71d765f09f409a66ee374, reversing
    changes made to 505d6004e4b2458f829a2b3d95843b243b67c394.

--- includes/csvStorage.php
+++ includes/csvStorage.php
@@ -1,22 +1,22 @@
 <?php
 // Written by Grant Storey on 11/10/17 from Lab 13
 
-// Include CSV Writing files
-include ("includes/KeboolaCSV/CsvFile.php");
-include ("includes/KeboolaCSV/Exception.php");
-include ("includes/KeboolaCSV/InvalidArgumentException.php");
-// get a file
-function csvGetFile($filePath) {
-	$path = __DIR__ . '/../' . 'data/comments.csv';
-	$csvFileObject = new Keboola\Csv\CsvFile($path);
-	return $csvFileObject;
-}
-// clear a file
-function csvClearFile($csvFile) {
-	$csvFile->clearFile($value);
-}
-// append to a file
-function csvAppendToFile($csvFile, $value) {
-	$csvFile->appendRow($value);
-}
+	// Include CSV Writing files
+	include ("includes/KeboolaCSV/CsvFile.php");
+	include ("includes/KeboolaCSV/Exception.php");
+	include ("includes/KeboolaCSV/InvalidArgumentException.php");
+	// get a file
+	function csvGetFile($filePath) {
+		$path = __DIR__ . '/../' . 'data/comments.csv';
+		$csvFileObject = new Keboola\Csv\CsvFile($path);
+		return $csvFileObject;
+	}
+	// clear a file
+	function csvClearFile($csvFile) {
+		$csvFile->clearFile($value);
+	}
+	// append to a file
+	function csvAppendToFile($csvFile, $value) {
+		$csvFile->appendRow($value);
+	}
 ?>
